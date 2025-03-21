2c0fbfb5016be6f16f26a7fd41195b61ea91665b
AsaneKento <74521188+AsaneKento@users.noreply.github.com>
2025-03-22 03:18:04 +0900

    feat: Vitestを追加した

---
+++ vitest.config.ts
@@ -0,0 +1,24 @@
+import react from "@vitejs/plugin-react"
+import tsconfigPaths from "vite-tsconfig-paths"
+import { defineConfig } from "vitest/config"
+
+export default defineConfig({
+  plugins: [tsconfigPaths(), react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: ["./src/tests/setupTests.ts"],
+    include: ["src/**/*.test.{ts,tsx}"],
+    exclude: ["src/lib/ui/**"],
+    coverage: {
+      provider: "v8",
+      include: ["src/**/*.{ts,tsx}"],
+      exclude: ["src/lib/ui/**"],
+      reporter: ["text", "html", "json-summary", "json"],
+      reportOnFailure: true,
+      thresholds: {
+        statements: 80,
+      },
+    },
+  },
+})
